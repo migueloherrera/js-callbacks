8714a7ea0c8dafeaa7437dd4387519db4e45e4a6
Miguel <miguel.o.herrera@gmail.com>
2016-07-21 16:00:12 -0500

    Callback exercises

---
+++ callbacks.js
@@ -0,0 +1,37 @@
+// Write a function, funcCaller, that takes a func (a function) and an arg (any data type). The function returns the func called with arg(as an argument).
+
+function funcCaller(callback, anyArg) {
+  return callback(anyArg);
+}
+
+function p(arg1) {
+  console.log(arr1);
+}
+
+funcCaller(p, 5+5); // 10
+
+// Write a function, firstVal, that takes an array, arr, and a function, func, and calls func with the first index of the arr, the index # and the whole array.
+
+function firstVal(arr, callback) {
+  return callback(arr[0], 0, arr);
+}
+
+function anyFunction(value, index, array) {
+  console.log("Value: " + value + " at Index: " + index + " From: " + array);
+}
+
+firstVal([5,4,3,2], anyFunction); // Value: 5 at Index: 0 From: 5,4,3,2
+
+// Change firstVal to work not only with arrays but also objects. Since objects are not ordered, you can use any key-value pair on the object.
+
+function firstVal(arr, callback) {
+  if (arr instanceof Array) {
+    return callback(arr[0], 0, arr);
+  } else {
+    for (var i in arr) {
+      return callback(arr[i], i, arr);
+    }
+  }
+}
+
+firstVal({'one': 1, 'two': 2}, anyFunction); // Value: 1 at Index: one From: [object Object]
